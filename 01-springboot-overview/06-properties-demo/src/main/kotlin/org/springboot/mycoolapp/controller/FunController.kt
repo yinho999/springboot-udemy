d4c10a6d45b79765fa553df0b3e0d6c724d4ccde
Naiker <yinho999@gmail.com>
2023-12-20 20:52:29 +0000

    Properties setup for spring boot via application.properties

--- 01-springboot-overview/06-properties-demo/src/main/kotlin/org/springboot/mycoolapp/controller/FunController.kt
+++ 01-springboot-overview/06-properties-demo/src/main/kotlin/org/springboot/mycoolapp/controller/FunController.kt
@@ -1,5 +1,6 @@
 package org.springboot.mycoolapp.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
